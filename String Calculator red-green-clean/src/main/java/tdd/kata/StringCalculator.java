76b07a9615301190d4a67b0b422732f25a258535
Le Ha Tu <tuleha52@gmail.com>
2013-07-11 14:01:23 +0700

    clean
    clean

--- String Calculator red-green-clean/src/main/java/tdd/kata/StringCalculator.java
+++ String Calculator red-green-clean/src/main/java/tdd/kata/StringCalculator.java
@@ -9,17 +9,20 @@ public class StringCalculator
     public int add(String numbers){
         if(numbers.trim().equals(""))
             return 0;
-        else{
-            if(numbers.contains(",")){
-                String []number = numbers.split(",");
-                int total = 0;
-                for(int i=0; i< number.length; i++){
-                    total +=Integer.parseInt(number[i]);
-                }
-                return total;
+        else if(numbers.contains(",")){
+            String []number = numbers.split(",");
+            int total = 0;
+            for(int i=0; i< number.length; i++){
+                total += convertString2Int(number[i]);
             }
-            else return Integer.parseInt(numbers);
+            return total;
         }
+        else return convertString2Int(numbers);
 
+    }
+
+    private int convertString2Int(String number)
+    {
+        return Integer.parseInt(number);
     }
 }
