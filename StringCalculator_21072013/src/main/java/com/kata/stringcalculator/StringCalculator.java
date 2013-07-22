adf8374a17f2844fd06758f7d7a8ba86a3aac15b
Le Ha Tu <tuleha52@gmail.com>
2013-07-22 09:50:04 +0700

    green

--- StringCalculator_21072013/src/main/java/com/kata/stringcalculator/StringCalculator.java
+++ StringCalculator_21072013/src/main/java/com/kata/stringcalculator/StringCalculator.java
@@ -1,5 +1,7 @@
 package com.kata.stringcalculator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,7 +13,7 @@ public class StringCalculator
 {
     private static final String DEFAULT_DELIMITER = ",|\n";
     private static final String DELIMITER_PATTERN = "//(.)\n(.*)";
-    public int add(String numbers)
+    public int add(String numbers) throws Exception
     {
         if (numbers.isEmpty())
         {
@@ -20,15 +22,35 @@ public class StringCalculator
         else
         {
             String[] arrayNumber = tokenizer(numbers);
+            List<Integer> negativeNumber = new ArrayList<Integer>();
             int total = 0;
+            int temp;
             for (int i = 0; i < arrayNumber.length; i++)
             {
-                total += string2IntConverter(arrayNumber[i]);
+                temp = string2IntConverter(arrayNumber[i]);
+                if(temp < 0)
+                {
+                    negativeNumber.add(temp);
+                }
+                else total += temp;
+            }
+            if(negativeNumber.size() > 0)
+            {
+                throw new Exception("negatives not allowed " + listToString(negativeNumber));
             }
             return total;
         }
     }
 
+    private String listToString(List<Integer> source)
+    {
+        String target = "";
+        for(Integer i: source)
+        {
+            target += i + " ";
+        }
+        return target;
+    }
     private int string2IntConverter(String numbers) throws NumberFormatException
     {
         return Integer.parseInt(numbers);
