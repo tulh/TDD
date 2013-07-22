35cb3ed6e41c97220ac747f6766d0d43063f7051
Le Ha Tu <tuleha52@gmail.com>
2013-07-22 07:30:17 +0700

    green

--- StringCalculator_21072013/src/main/java/com/kata/stringcalculator/StringCalculator.java
+++ StringCalculator_21072013/src/main/java/com/kata/stringcalculator/StringCalculator.java
@@ -1,13 +1,16 @@
 package com.kata.stringcalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * User: Tu
  * Date: 7/22/13
  */
 public class StringCalculator
 {
-    private static final String DELIMITER = ",|\n";
-
+    private static final String DEFAULT_DELIMITER = ",|\n";
+    private static final String DELIMITER_PATTERN = "//(.)\n(.*)";
     public int add(String numbers)
     {
         if (numbers.isEmpty())
@@ -16,7 +19,18 @@ public class StringCalculator
         }
         else
         {
-            String[] arrayNumber = numbers.split(DELIMITER);
+            String[] arrayNumber;
+            if(numbers.startsWith("//"))
+            {
+                Pattern p = Pattern.compile(DELIMITER_PATTERN);
+                Matcher m = p.matcher(numbers);
+                m.matches();
+                String delimiter = m.group(1);
+                String number = m.group(2);
+                arrayNumber = number.split(delimiter);
+
+            }
+            else arrayNumber = numbers.split(DEFAULT_DELIMITER);
             int total = 0;
             for (int i = 0; i < arrayNumber.length; i++)
             {
