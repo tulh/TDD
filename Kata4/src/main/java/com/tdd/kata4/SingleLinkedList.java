6443575ec88ae75e69f4238f74374374af4f14ce
Le Ha Tu <tuleha52@gmail.com>
2013-08-02 09:01:21 +0700

    green
    green 4.1 add class Node, declared properties for SingleLinkedList

--- Kata4/src/main/java/com/tdd/kata4/SingleLinkedList.java
+++ Kata4/src/main/java/com/tdd/kata4/SingleLinkedList.java
@@ -6,4 +6,52 @@ package com.tdd.kata4;
  */
 public class SingleLinkedList
 {
+    public int size;
+    private Node node;
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public void setSize(int size)
+    {
+        this.size = size;
+    }
+
+    public Node getNode()
+    {
+        return node;
+    }
+
+    public void setNode(Node node)
+    {
+        this.node = node;
+    }
+
+    private static class Node
+    {
+        private Node next;
+        private Object e;
+
+        private Node getNext()
+        {
+            return next;
+        }
+
+        private void setNext(Node next)
+        {
+            this.next = next;
+        }
+
+        private Object getE()
+        {
+            return e;
+        }
+
+        private void setE(Object e)
+        {
+            this.e = e;
+        }
+    }
 }
