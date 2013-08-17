3f074598b73e65e4b1ebbfa3c327693039492b40
hunter lee <huynhlbq@gmail.com>
2013-08-17 11:50:30 +0700

    PASS Test 2

--- TicTacToe/grails-app/controllers/tictactoe/GameController.groovy
+++ TicTacToe/grails-app/controllers/tictactoe/GameController.groovy
@@ -5,18 +5,18 @@ class GameController {
     def Board board
     def Character player1
     def Character player2
+    def Character activePlayer
 
     def startGame = {
-        if(params.player=='x')
-        {
+        if (params.player == 'x') {
             player1 = Character.X
             player2 = Character.O
-        }
-        else
-        {
+        } else {
             player1 = Character.O
             player2 = Character.X
         }
+        //set player1 active
+        activePlayer = player1
         board = boardService.initAllCell()
         render(params.player + " move first")
     }
@@ -26,9 +26,14 @@ class GameController {
     }
 
     def move = {
-        Cell cell = boardService.findCellByRowAndCol(params.row, params.col)
-        boardService.updateCell(cell, "")
+        Cell cell = boardService.findCellByRowAndCol(Integer.parseInt(params.row), Integer.parseInt(params.col))
+        boardService.updateCell(cell, activePlayer.toString())
+        changeActivePlayer()
         render params.row + "," + params.col;
+    }
+
+    def changeActivePlayer() {
+        activePlayer = (activePlayer == player1) ? player2 : player1
     }
 
     def waitsForMove = {
