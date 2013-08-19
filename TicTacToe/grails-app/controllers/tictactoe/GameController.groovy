0d127c0595d257a5b3b7febda8d33652ad142b0f
Le Ha Tu <tuleha52@gmail.com>
2013-08-19 09:41:49 +0700

    refactor: move game logic to BoardService class

--- TicTacToe/grails-app/controllers/tictactoe/GameController.groovy
+++ TicTacToe/grails-app/controllers/tictactoe/GameController.groovy
@@ -28,15 +28,17 @@ class GameController
         boardService.updateCell(cell, activePlayer)
         render params.row + "," + params.col
         println activePlayer
-        if (getWinner()!=null)
+        def winner = boardService.getWinner(board)
+        def fullBoard = boardService.isBoardFull(board)
+        if(winner != null)
         {
-            println getWinner() + " win"
-            render(getWinner() + " win")
+            println winner + " win"
+            render(winner + " win")
             stopGame()
         }
-        else if (isBoardFull())
+        if(fullBoard)
         {
-            println("full!")
+            println "fullBoard: " + fullBoard
             render("full!")
             stopGame()
         }
@@ -49,49 +51,6 @@ class GameController
         activePlayer = (activePlayer == X) ? O : X
     }
 
-    static boolean isBoardFull()
-    {
-        for (Cell cell : board)
-        {
-            if (cell.value == null)
-            {
-                return false
-            }
-        }
-        println "FULL!!!"
-        return true
-    }
-
-    String getWinner() {
-        Cell cell1 = boardService.findCellByRowAndCol(1,1,board)
-        Cell cell2 = boardService.findCellByRowAndCol(1,2,board)
-        Cell cell3 = boardService.findCellByRowAndCol(1,3,board)
-        Cell cell4 = boardService.findCellByRowAndCol(2,1,board)
-        Cell cell5 = boardService.findCellByRowAndCol(2,2,board)
-        Cell cell6 = boardService.findCellByRowAndCol(2,3,board)
-        Cell cell7 = boardService.findCellByRowAndCol(3,1,board)
-        Cell cell8 = boardService.findCellByRowAndCol(3,2,board)
-        Cell cell9 = boardService.findCellByRowAndCol(3,3,board)
-        // horizontal line
-        if(isAllLike(cell1, cell2, cell3)) return cell1.value
-        if(isAllLike(cell4, cell5, cell6)) return cell4.value
-        if(isAllLike(cell7, cell8, cell9)) return cell7.value
-        // vertical line
-        if(isAllLike(cell1, cell4, cell7)) return cell1.value
-        if(isAllLike(cell2, cell5, cell8)) return cell2.value
-        if(isAllLike(cell3, cell6, cell9)) return cell3.value
-        // left diagonal line
-        if(isAllLike(cell1, cell5, cell9)) return cell1.value
-        // right diagonal line
-        if(isAllLike(cell7, cell5, cell3)) return cell7.value
-        return null
-    }
-
-    private boolean isAllLike(Cell cell1, Cell cell2, Cell cell3)
-    {
-        return (cell1.value!=null)&&(cell2.value!=null)&&(cell3.value!=null)&&
-                (cell1.value == cell2.value)&&(cell1.value==cell3.value)&&(cell2.value==cell3.value)
-    }
     def save = {
         Cell cell = new Cell(row: 1, col: 1, value: "x")
         cell.save(flush: true)
--- TicTacToe/grails-app/services/tictactoe/BoardService.groovy
+++ TicTacToe/grails-app/services/tictactoe/BoardService.groovy
@@ -18,6 +18,50 @@ class BoardService {
 
     }
 
+    boolean isBoardFull(board)
+    {
+        for (Cell cell : board)
+        {
+            if (cell.value == null)
+            {
+                return false
+            }
+        }
+        println "FULL!!!"
+        return true
+    }
+
+    String getWinner(def board) {
+        Cell cell1 = findCellByRowAndCol(1,1,board)
+        Cell cell2 = findCellByRowAndCol(1,2,board)
+        Cell cell3 = findCellByRowAndCol(1,3,board)
+        Cell cell4 = findCellByRowAndCol(2,1,board)
+        Cell cell5 = findCellByRowAndCol(2,2,board)
+        Cell cell6 = findCellByRowAndCol(2,3,board)
+        Cell cell7 = findCellByRowAndCol(3,1,board)
+        Cell cell8 = findCellByRowAndCol(3,2,board)
+        Cell cell9 = findCellByRowAndCol(3,3,board)
+        // horizontal line
+        if(isAllLike(cell1, cell2, cell3)) return cell1.value
+        if(isAllLike(cell4, cell5, cell6)) return cell4.value
+        if(isAllLike(cell7, cell8, cell9)) return cell7.value
+        // vertical line
+        if(isAllLike(cell1, cell4, cell7)) return cell1.value
+        if(isAllLike(cell2, cell5, cell8)) return cell2.value
+        if(isAllLike(cell3, cell6, cell9)) return cell3.value
+        // left diagonal line
+        if(isAllLike(cell1, cell5, cell9)) return cell1.value
+        // right diagonal line
+        if(isAllLike(cell7, cell5, cell3)) return cell7.value
+        return null
+    }
+
+    private boolean isAllLike(Cell cell1, Cell cell2, Cell cell3)
+    {
+        return (cell1.value!=null)&&(cell2.value!=null)&&(cell3.value!=null)&&
+                (cell1.value == cell2.value)&&(cell1.value==cell3.value)&&(cell2.value==cell3.value)
+    }
+
     List<Cell> initAllCell(List<Cell> board) {
         for (int row = 1; row <= 3; row++) {
             for (int col = 1; col <= 3; col++) {
